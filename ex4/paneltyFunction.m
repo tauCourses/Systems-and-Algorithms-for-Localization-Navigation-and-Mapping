8c23ecc74b5587d83a18bab60206a904bfbb9bc8
MatanLed <matan.harel.mh@gmail.com>
2018-04-14 10:05:02 +0300

    improve 4 assignment

--- ex4/paneltyFunction.m
+++ ex4/paneltyFunction.m
@@ -4,7 +4,8 @@ c = 3;
 n = size(R,2);
 for ii=1:n
     panelty = panelty + (norm(R(:, ii)-p)-norm(R(:, ii)-t) + rand(1,1))^2;
-   % 5/6 - panelty = panelty + (norm(R(:, ii)-p)-norm(R(:, ii)-t) + rand(1,1) + normrnd(0,100))^2;
+   % 5/6 with randomness:
+   % panelty = panelty + (norm(R(:, ii)-p)-norm(R(:, ii)-t) + rand(1,1) + normrnd(0,100))^2;
 end
 
 panelty = panelty/(c^2);
